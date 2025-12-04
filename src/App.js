1d6f0e4ca80459b08978f4906bb8d7f331b279c9
DeverLoper513 <151453624+DeverLoper513@users.noreply.github.com>
2025-12-04 11:35:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "1252620";
 
   //Product ID
-  let ProductId = "862935333";
+  let ProductId = "14897380655436";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
